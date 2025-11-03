986c54aab90171b1676a700e9e0ebff5dbe9573c
Vhuhwavho <denganetshi@gmail.com>
2025-11-03 10:46:02 +0200

    init

---
+++ src/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+from flask_login import LoginManager
+from .models import db, User
+import os
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'a-very-secret-key'
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, '../resources/users.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    from .auth import auth_bp
+    app.register_blueprint(auth_bp)
+
+    @app.route('/')
+    def dashboard():
+        """lending page"""
+        return render_template('index.html')
+
+    with app.app_context():
+        db.create_all()
+
+    return app
--- src/main.py
+++ src/main.py
@@ -3,32 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
+from . import create_app
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'a-very-secret-key'
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, '../resources/users.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app = create_app()
 
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    email = db.Column(db.String(150), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
 
 @app.route('/')
 def dashboard():
@@ -81,8 +61,3 @@ def register():
         flash('Registration successful! Please log in.')
         return redirect(url_for('login'))
     return render_template('register.html')
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(host='0.0.0.0', debug=True)
